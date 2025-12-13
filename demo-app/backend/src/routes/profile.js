b63c36b4299802bf0c9056c95a85264047ed7a37
saha-7op <saha7pritam@gmail.com>
2025-12-14 01:13:46 +0530

    Trigger test [trigger-failure]

--- demo-app/backend/src/routes/profile.js
+++ demo-app/backend/src/routes/profile.js
@@ -11,7 +11,7 @@ const profileRouter = express.Router()
 profileRouter.get("/profile/view", userAuth, async (req, res) => {
    try{
     
-    // These are old redundent codes, which  currently handled by userAuth middleware
+    // These old redundent codes, which  currently handled by userAuth middleware
 
 
 // const cookies = req.cookies
